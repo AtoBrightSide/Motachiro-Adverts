bd079f29affbf8b2cbe47007783e725df8938565
Betu55 <se.bemenet.semegnew@gmail.com>
2021-02-06 07:01:37 +0300

    Working on the UI with demo images

---
+++ Assets/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+$(document).ready(function(){
+    $('.tabs').tabs();
+  });
+
+(function($){
+$(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+}); // end of document ready
+})(jQuery); // end of jQuery name space
+  
