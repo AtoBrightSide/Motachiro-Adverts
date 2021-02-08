1086c3c3b735a2b70080c65f60dfd9ace811d40b
Abel Mesele <meseleabel99@gmail.com>
2021-02-08 12:42:12 +0300

    Finished working on the accounts

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -11,7 +11,19 @@ $(function(){
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.collapsible').collapsible();
 
 }); // end of document ready
 })(jQuery); // end of jQuery name space
   
+// var userName = document.getElementById("#user-name");
+// var hd = document.querySelector(".header");
+// var crsl = document.querySelector(".carousel");
+
+// var temp = false
+
+
+// if (!temp) {
+//     // hd.innerHTML = ""
+//     crsl.style.display = "none"; 
+// }
