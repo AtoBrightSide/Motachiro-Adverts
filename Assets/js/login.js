ce75568d9f7bc2c52c0f33457cc0f8b31e308e2d
Betu55 <se.bemenet.semegnew@gmail.com>
2021-02-19 15:03:39 +0300

    The login verification isssue has been solved.

--- Assets/js/login.js
+++ Assets/js/login.js
@@ -1,66 +1,60 @@
+//authorization js file
 var loginButton = document.getElementById("loginButton");
 var theuserName = document.getElementById("email");
 var thepword = document.getElementById("password");
 var loginMessage = document.getElementById("loginMessage");
+
 var count = 0;
 var actualLength = 0;
 
-loginButton.addEventListener('click', function () {
+loginButton.addEventListener('click', function (e) {
+    e.preventDefault();
     let trs = DB.transaction(['LoginInfo'], 'readonly');
     let obj5 = trs.objectStore('LoginInfo');
-    // let myIndex = obj5.index('userLogin');
-    // var myCounter = myIndex.count();
-    
-    // myCounter.onsuccess = function () {
-    //     var theCount = myCounter.result;   
-    // }
-    let res = obj5.count();
+    var res = obj5.count();
+
     res.onsuccess = function () {
         count = res.result;
-    }
+    };
+
     obj5.openCursor().onsuccess = function (e) {
-
         var cursor = e.target.result;
-        let u = cursor.value.userLogin.split("*");
-        let uN = u[0];
-        let pN = u[1];
 
-        if (cursor){
-            if((uN==theuserName.value) && (pN==thepword.value)){
+        if (cursor) {
+            let u = cursor.value.userLogin.split("*");
+            let uN = u[0];
+            let pN = u[1];
+            if ((uN == theuserName.value) && (pN == thepword.value)) {
                 // console.log("there it is")
                 location.assign("accounts.html")
-                userName.value = "";
-                thepword.value = "";
+                theuserName.value = '';
+                thepword.value = '';
                 count = 0;
-                actualLength = 0
-                return;
+                actualLength = 0;
+                return 0;
             }
-            
-            else{
-                // let li = document.createElement("p");
-                // li.className = "red-text text-darken-2";
-                // li.innerHTML = "Incorrect entry, try again?";
-                // alert("Invalid Entry")
-                // thepword.style.borderColor = "red";
-                // theuserName.style.borderColor = "red";
-                // loginMessage.appendChild(li);
-                
-                actualLength++;
-            
+
+            else {
+
+                actualLength++
             }
             cursor.continue();
-                
-        }else {
+        }
+        else {
             if (count == actualLength) {
-                console.log("No data in DB");
+                console.log("NO DATA IN DB");
                 count = 0;
                 actualLength = 0;
                 thepword.style.borderColor = "red";
                 theuserName.style.borderColor = "red";
+                loginMessage.className = "red-text text-accent-2";
+                loginMessage.innerHTML = "Incorrect entry, try again?";
             }
         }
-        
-    }
 
-    
+    };
+
+
+
 });
+
