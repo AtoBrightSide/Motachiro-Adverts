615bda2b3d4cc2673b472eee551767cb1e09ccae
Betu55 <se.bemenet.semegnew@gmail.com>
2021-02-19 18:35:33 +0300

    Admin login form is now functional.

--- Assets/js/inits.js
+++ Assets/js/inits.js
@@ -5,6 +5,7 @@
         $('.sidenav').sidenav();
         $('.parallax').parallax();
         $('.collapsible').collapsible();
+        $('.dropdown-trigger').dropdown();
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
--- Assets/js/login.js
+++ Assets/js/login.js
@@ -1,11 +1,13 @@
 //authorization js file
 var loginButton = document.getElementById("loginButton");
+var loginButtonAdmin = document.getElementById("loginButtonAdmin");
 var theuserName = document.getElementById("email");
 var thepword = document.getElementById("password");
 var loginMessage = document.getElementById("loginMessage");
 
 var count = 0;
 var actualLength = 0;
+
 
 loginButton.addEventListener('click', function (e) {
     e.preventDefault();
@@ -58,3 +60,51 @@ loginButton.addEventListener('click', function (e) {
 
 });
 
+loginButtonAdmin.addEventListener('click', function (e) {
+    e.preventDefault();
+    let trs = DB.transaction(['AdminInfo'], 'readonly');
+    let obj5 = trs.objectStore('AdminInfo');
+    var res = obj5.count();
+
+    res.onsuccess = function () {
+        count = res.result;
+    };
+
+    obj5.openCursor().onsuccess = function (e) {
+        var cursor = e.target.result;
+
+        if (cursor) {
+            let u = cursor.value.userLogin.split("*");
+            let uN = u[0];
+            let pN = u[1];
+            if ((uN == theuserName.value) && (pN == thepword.value)) {
+                // console.log("there it is")
+                location.assign("accounts.html")
+                theuserName.value = '';
+                thepword.value = '';
+                count = 0;
+                actualLength = 0;
+                return 0;
+            }
+
+            else {
+
+                actualLength++
+            }
+            cursor.continue();
+        }
+        else {
+            if (count == actualLength) {
+                console.log("NO DATA IN DB");
+                count = 0;
+                actualLength = 0;
+                thepword.style.borderColor = "red";
+                theuserName.style.borderColor = "red";
+                loginMessage.className = "red-text text-accent-2";
+                loginMessage.innerHTML = "Incorrect entry, try again?";
+            }
+        }
+
+    };
+
+});
