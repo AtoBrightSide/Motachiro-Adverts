9641378bb39c2da31142e557ed1087571c6cd500
Betu55 <se.bemenet.semegnew@gmail.com>
2021-02-20 11:54:42 +0300

    A decryption function has been implemented to enble the system to recognize passwords during login.

--- Assets/js/encrypt_decrypt.js
+++ Assets/js/encrypt_decrypt.js
@@ -1,4 +1,7 @@
+//this is the an important factor in the encrypting process
 const increment = 55;
+
+//To encrypt the strings
 function simpleEncrypt(string) {
     let capsule = 'key@enc~'
     for (let i = 0; i < string.length; i++) {
@@ -7,4 +10,12 @@ function simpleEncrypt(string) {
     return capsule + 'key@enc';
 }
 
-
+//To decrypt the encrypted string.
+function simpleDecrypt(string) {
+    var passSplit = string.split('~');
+    let passString = ''
+    for (let i = 1; i < passSplit.length - 1; i++) {
+        passString += (String.fromCharCode(parseInt(passSplit[i]) - increment))
+    }
+    return passString;
+}
